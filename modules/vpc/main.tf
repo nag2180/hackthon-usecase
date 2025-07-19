dc67aafcebb8a57677f62a685b1609f6fb720376
nag2180 <vnagaraja362@gmail.com>
2025-07-19 09:43:56 +0000

    update cidr

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -47,31 +47,31 @@ resource "aws_subnet" "private" {
   }
 }
 
-## Elastic IPs for NAT Gateways
-#resource "aws_eip" "nat" {
-#  count = length(var.availability_zones)
-#
-#  domain = "vpc"
-#  depends_on = [aws_internet_gateway.main]
-#
-#  tags = {
-#    Name = "${var.project_name}-${var.environment}-nat-eip-${count.index + 1}"
-#  }
-#}
-#
-## NAT Gateways
-#resource "aws_nat_gateway" "main" {
-#  count = length(var.availability_zones)
-#
-#  allocation_id = aws_eip.nat[count.index].id
-#  subnet_id     = aws_subnet.public[count.index].id
-#
-#  tags = {
-#    Name = "${var.project_name}-${var.environment}-nat-gateway-${count.index + 1}"
-#  }
-#
-#  depends_on = [aws_internet_gateway.main]
-#}
+# Elastic IPs for NAT Gateways
+resource "aws_eip" "nat" {
+  count = length(var.availability_zones)
+
+  domain = "vpc"
+  depends_on = [aws_internet_gateway.main]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-nat-eip-${count.index + 1}"
+  }
+}
+
+# NAT Gateways
+resource "aws_nat_gateway" "main" {
+  count = length(var.availability_zones)
+
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-nat-gateway-${count.index + 1}"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
 
 # Route Table for Public Subnets
 resource "aws_route_table" "public" {
@@ -95,7 +95,7 @@ resource "aws_route_table" "private" {
 
   route {
     cidr_block     = "0.0.0.0/0"
-    #nat_gateway_id = aws_nat_gateway.main[count.index].id
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
   }
 
   tags = {
