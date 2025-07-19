9de709fe2e9d02486d9b08a357db4063ca664b42
nag2180 <vnagaraja362@gmail.com>
2025-07-19 08:01:52 +0000

    update cidr

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "project_name" {
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "141.0.0.0/16"
 }
 
 variable "availability_zones" {
